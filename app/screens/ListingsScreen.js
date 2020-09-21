581e94effdbc04afd08330f86b359694f729efc1
Sargon <Sargonpoli@hotmail.com>
2020-09-21 20:56:48 +0200

    ...

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,45 +1,66 @@
 import React from "react";
 import { FlatList, StyleSheet } from "react-native";
+import ReactDOM from "react-dom";
 
 import Card from "../components/Card";
 import colors from "../config/colors";
 import routes from "../navigation/routes";
 import Screen from "../components/Screen";
 import Filter from "../components/Filter";
+import FirebaseConfig from "../firebase/config";
 
-const listings = [
-  {
-    id: 1,
-    title: "Vacker & charmig bostad",
-    price: 2500000,
-    propertySize: 105,
-    room: 4,
-    image: require("../assets/house01.jpg"),
-  },
-  {
-    id: 2,
-    title: "Lägenhet med utsikt",
-    price: 1500000,
-    propertySize: 105,
-    room: 2,
-    image: require("../assets/house02.jpg"),
-  },
-];
+import * as firebase from "firebase";
+
+const db = firebase.firestore();
+
+var allListings = [];
+
+db.collection("Listings")
+  .get()
+  .then((snapshot) => {
+    snapshot.docs.forEach((doc) => {
+      allListings.push(doc.data());
+      console.log(allListings);
+    });
+  });
+
+// const listings = [
+//   {
+//     id: 1,
+//     title: "Vacker & charmig bostad",
+//     price: 2500000,
+//     propertySize: 105,
+//     room: 4,
+//     image: require("../assets/house01.jpg"),
+//   },
+//   {
+//     id: 2,
+//     title: "Lägenhet med utsikt",
+//     price: 1500000,
+//     propertySize: 105,
+//     room: 2,
+//     image: require("../assets/house02.jpg"),
+//   },
+//   {
+//     id: 4,
+//     title: "Lägenhet med utsikt",
+//     price: 1500000,
+//     propertySize: 105,
+//     room: 2,
+//     image: require("../assets/house02.jpg"),
+//   },
+// ];
 
 function ListingsScreen({ navigation }) {
   return (
     <Screen style={styles.screen}>
       <Filter />
       <FlatList
-        data={listings}
-        keyExtractor={(listing) => listing.id.toString()}
+        data={allListings}
+        keyExtractor={(listing) => listing.Id.toString()}
         renderItem={({ item }) => (
           <Card
-            title={item.title}
-            subTitle={item.price + " kr"}
-            propSize={item.propertySize + " kvm"}
-            room={item.room + " Rum"}
-            image={item.image}
+            title={item.Title}
             onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
           />
         )}
